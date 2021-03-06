3e9deb453903cef48968194775f50c00d00328d2
Damian <ChetanSapa@gmail.com>
2021-03-06 19:25:26 +0100

    rebuild with localStorage my Blocknout/Shoppiinglist.

--- Shoppinglist/script/script.js
+++ Shoppinglist/script/script.js
@@ -2,16 +2,61 @@ let taskText = document.getElementById("taskText");
 let addTask = document.getElementById('addTask');
 let taskList = document.getElementById('taskList');
 
-addTask.addEventListener('click', function() {
-    var li = document.createElement('li');
-    li.innerText = taskText.value;
-    taskList.appendChild(li);
+let todoList = [];
+
+if (localStorage.getItem('todo')) {
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessages();
+}
+
+addTask.addEventListener('click', function () {
+
+    let newTodo = {
+        todo: taskText.value
+    };
+
+    todoList.push(newTodo);
+    displayMessages();
     taskText.value = '';
-    li.addEventListener('click', function () {
-        li.classList.add('cool')
-        li.style.textDecoration = 'line-through';
-    })
-    li.addEventListener('dblclick', function () {
-        taskList.removeChild(li);
-    })
+    localStorage.setItem('todo', JSON.stringify(todoList));
+
+
+    // let liEl = document.createElement('li');
+    // liEl.innerText = taskText.value;
+    // taskList.appendChild(liEl);
+    // taskText.value = '';
+    // liEl.addEventListener('click', function () {
+    //     liEl.classList.add('cool')
+    //     liEl.style.textDecoration = 'line-through';
+    // })
+    // liEl.addEventListener('dblclick', function () {
+    //     taskList.removeChild(liEl);
+    // })
 });
+
+function displayMessages() {
+    let displayMessage = '';
+    if (todoList.length === 0) taskList.innerHTML = '';
+    todoList.forEach(function (item, i) {
+        displayMessage += `
+        <li id="${i}">
+            ${item.todo}
+        </li>
+        `;
+        taskList.innerHTML = displayMessage;
+    });
+}
+
+taskList.addEventListener('click', function (event) {
+    event.preventDefault()
+    let liEl = event.target;
+    liEl.classList.add('cool')
+    liEl.style.textDecoration = 'line-through';
+})
+
+taskList.addEventListener("dblclick", function (event) {
+        let i = event.target.id;
+        todoList.splice(i, 1);
+        displayMessages();
+        localStorage.setItem('todo', JSON.stringify(todoList));
+})
